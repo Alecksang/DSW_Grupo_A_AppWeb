7158802894c592be3e71abdaef7bb1374848532f
Alecksang <asangiacomo.17@est.ucab.edu.ve>
2023-11-12 23:12:27 -0400

    Se hizo la clase Mapper para Usuarios: UsuarioMapper

--- src/persistence/dao/src/main/java/com/ucab/cmcapp/persistence/DaoFactory.java
+++ src/persistence/dao/src/main/java/com/ucab/cmcapp/persistence/DaoFactory.java
@@ -10,5 +10,8 @@ public class DaoFactory
     {
         return new UserDao( handler );
     }
-
+    public static UsuarioDao createUsuarioDao( DBHandler handler )
+    {
+        return new UsuarioDao( handler );
+    }
 }
